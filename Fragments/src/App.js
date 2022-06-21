d4678a90147439bdcf10f74684ab8bb0005e90e1
Stanislav Krasichinsky <thekrasich@gmail.com>
2022-06-21 19:06:52 +0300

    Merge

--- Fragments/src/App.js
+++ Fragments/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/header/Header';
-import Registration from './components/registration/RegistrationForm';
+import Registration from './components/registration/RegistrationForm'
 import SignUp from './components/registration/SignUpForm';
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import {gapi} from 'gapi-script';
@@ -20,13 +20,12 @@ function App() {
   });
   return (
     <Router>
-      <Header/>
-      <Routes>
-          <Route path="/" element = {<SignUp/>} exact/>
-          <Route path="/Details" element = {<Registration/>} exact/>
-          <Route path="/ProfileEdit" element = {<ProfileEdit/>} exact/>
-      </Routes>
-    </Router>
+    <Header/>
+    <Routes>
+        <Route path="/" element = {<SignUp/>} exact/>
+        <Route path="/Details" element = {<Registration/>} exact/>
+    </Routes>
+  </Router>
   );
 }
 export default App;
