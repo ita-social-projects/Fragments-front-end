8619677eaf41a5df904ce221786d2e430df40f80
Vladyslavko36 <103031454+Vladyslavko36@users.noreply.github.com>
2022-07-25 23:57:46 +0300

    Closes #053 - Allow user to Sign in the website UI (#16)
    * Added sign in with JWT token
    
    * Added Services folder with axios requests
    
    * Added Service with axios request
    
    * Header updates
    
    * Facebook and Google auth
    
    * Updated Routes
    
    * Update userService.js
    
    * Update RegistrationForm.jsx
    
    * Update SignUpForm.jsx
    
    * Sonar bugs fixes

--- Fragments/src/App.js
+++ Fragments/src/App.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/header/Header";
 import Profile from "./components/profile page/profile/Profile";
 import RegistrationForm from "./components/registration/RegistrationForm";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {getUser} from './services/userService';
+import SignUp from './components/SignUp/SignUpForm'
 
 function App() {
+  const [user, setUser] = useState();
+
+  useEffect(()=>{
+    const token = window.localStorage.getItem('token');
+    if(!user)
+      getUser(token, setUser)
+  },[user]);
+  
   return (
     <Router>
-      <Header />
+      <Header user = {user}/>
       <Routes>
+        <Route path="/Sign-up" element = {<SignUp/>} exact/>
         <Route path="/Registration" element={<RegistrationForm />} />
         <Route path="/api/Users" element={<Profile />} />
       </Routes>
---
+++ Fragments/src/components/SignUp/SignUpForm.jsx
@@ -0,0 +1,125 @@
+import React from "react";
+import FacebookLogin from "react-facebook-login";
+import { useLocation, useNavigate } from "react-router-dom";
+import styles from "../UI/signup.module.scss";
+import { GoogleLogin } from "react-google-login";
+import { loginUser } from "../../services/userService";
+
+const clientId =
+  "366436901363-b93c7i7nj1rmvnle6m992dgecfsf4bcd.apps.googleusercontent.com";
+const FacebookAppId = "505126904671302";
+
+const LoginForm = () => {
+  const navigate = useNavigate();
+  const action = useLocation().state;
+  const state = {
+    isLoggedIn: false,
+    userId: "",
+    fullname: "",
+    email: "",
+    picture: "",
+  };
+  const responseFacebook = (response) => {
+    state.isLoggedIn = true;
+    state.userId = response.id;
+    state.fullname = response.name;
+    state.email = response.email;
+    state.picture = response.picture.data.url;
+    if (action) {
+      loginUser({ email: state.email });
+      navigate("/", { state: null });
+    } else navigate("/Registration", { state: state });
+  };
+  const responseSuccessGoogle = (response) => {
+    console.log("Login is success", response.profileObj);
+    state.userId = response.profileObj.googleId;
+    state.isLoggedIn = true;
+    state.email = response.profileObj.email;
+    state.fullname =
+      response.profileObj.givenName + response.profileObj.familyName;
+    state.picture = response.profileObj.imageUrl;
+    if (action) {
+      loginUser({ email: state.email });
+      navigate("/", { state: null });
+    } else navigate("/Registration", { state: state });
+  };
+  const responseFailureGoogle = (response) => {
+    console.log("Login is failed");
+  };
+  return (
+    <div className={styles.container + " registration-container"}>
+      <div className={styles.row}>
+        <div className={styles.moduleborderwrap}>
+          <form className={styles.registrationForm}>
+            <legend>Зареєструватись</legend>
+            <div className={styles.hints}>Створіть обліковий запис</div>
+            <div className={styles.controlGroup}>
+              <div className={styles.controls}>
+                <GoogleLogin
+                  clientId={clientId}
+                  render={(renderProps) => (
+                    <button
+                      onClick={renderProps.onClick}
+                      className={styles.logbutton}
+                    >
+                      {" "}
+                      <img
+                        className={styles.svg1}
+                        src="GoogleLogo.svg"
+                        alt="Google"
+                      />{" "}
+                      Продовжити через Google
+                    </button>
+                  )}
+                  onSuccess={responseSuccessGoogle}
+                  onFailure={responseFailureGoogle}
+                  cookiePolicy={"single_host_origin"}
+                />
+                <div className={styles.controls}>
+                  <FacebookLogin
+                    cssClass={styles.logbutton}
+                    icon={
+                      <img
+                        className={styles.svg1}
+                        src="Facebook.svg"
+                        alt="filter applied"
+                      />
+                    }
+                    appId={FacebookAppId}
+                    autoLoad={true}
+                    cookie={true}
+                    callback={responseFacebook}
+                    textButton="Продовжити через Facebook"
+                    fields="name,email,picture"
+                  />
+                </div>
+              </div>
+              <div className={styles.controls}>
+                <button className={styles.logbutton}>
+                  <img
+                    className={styles.svg1}
+                    src="Instagram.svg"
+                    alt="Facebook"
+                  />{" "}
+                  Продовжити через Instagram
+                </button>
+              </div>
+            </div>
+
+            <hr className={styles.line} />
+            <div className={styles.hints}>
+              Вже маєте обліковий запис?{" "}
+              <a href="http://localhost:3000/">Увійти до системи</a>
+            </div>
+            <div className={styles.hints}>
+              Продовжуючи, ти приймаєш{" "}
+              <a href="http://localhost:3000/">”Умови Спільноти”</a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
