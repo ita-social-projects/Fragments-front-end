a8b3a82afa9c9a6b9959372aa32154bc0c17a0cc
Vladyslav Leskiv <vlad2199944@gmail.com>
2022-08-02 22:34:18 +0300

    Update Logged.jsx

--- Fragments/src/components/header/Logged.jsx
+++ Fragments/src/components/header/Logged.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { useState } from "react";
+import { hub } from "../../services/notificationService";
 
 const Logged = ({ ...props }) => {
+  const [notify, setNotify] = useState()
+  hub.hubConnection.on("createNotify", (message) => {
+    setNotify(message)
+    console.log(message);
+  },[]);
   return (
     <div className={props.className.userLogged}>
       <span className={props.className.projects}>Мої проекти</span>
       <span className={props.className.searchIcon}>
-        <img src="/bellnotification.svg" alt="notif" />
+        {notify ? <img src="/bellnotification.svg" alt="bell" /> : <img src="/bell.svg" alt="noBell" />}
       </span>
       <div className={props.className.profile}>
         <img
