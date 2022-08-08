c3a00442f8e15d97c5874a0627016ba884123555
Dammy99 <deslyly.prod@gmail.com>
2022-08-08 15:13:15 +0300

    fix and replaced getmethod

--- Fragments/src/__tests__/getmethodData/getmethod.test.jsx
+++ Fragments/src/__tests__/getmethodData/getmethod.test.jsx
@@ -1,37 +1,38 @@
 import "@testing-library/jest-dom";
 import axios from "axios";
-import getDataFromApi from "../../components/getmethod/getDataFromApi";
+import getDataFromApi from "../../components/profile page/getmethod/getDataFromApi";
 jest.mock("axios");
-jest.mock("../../components/getmethod/getmethod");
-
+jest.mock("../../components/profile page/getmethod/getDataFromApi");
 
 describe("Get methdod gets info from api:", () => {
   afterEach(() => {
     jest.resetModules();
   });
-  it("should return correct response when getGitHubUser is called and no error occur", async () => {
-
+  it("should return correct response when user gets data", async () => {
     const response = { id: 2, fullName: "Andrew Joe" };
     const personid = 2;
     const getNotes = jest.fn();
     axios.get.mockResolvedValueOnce(response);
-    const result = getDataFromApi({getNotes,personid});
-    expect(result).toEqual(response);
-    jest.unmock("../../components/getmethod/getmethod");
+    const result = getDataFromApi({ getNotes, personid });
+    expect(result).resolves.toEqual(response);
+    jest.unmock("../../components/profile page/getmethod/getDataFromApi");
   });
 
-    it("Should return from getData method correct data", async () => {
-      jest.doMock("../.././components/getmethod/getmethod", () => {
+  it("Should return from getData method correct data", async () => {
+    jest.doMock(
+      "../../components/profile page/getmethod/getDataFromApi",
+      () => {
         return {
           id: 2,
           fullName: "Vitaliy Testing",
         };
-      });
-      const response = { id: 2, fullName: "Vitaliy Testing" };
-      return import("../../components/getmethod/getDataFromApi").then((moduleName) => {
-        expect(moduleName.default).toEqual(response);
-      });
+      }
+    );
+    const response = { id: 2, fullName: "Vitaliy Testing" };
+    return import(
+      "../../components/profile page/getmethod/getDataFromApi"
+    ).then((moduleName) => {
+      expect(moduleName.default).toEqual(response);
     });
+  });
 });
-
-
