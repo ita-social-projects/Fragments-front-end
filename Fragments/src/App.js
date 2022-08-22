4a7476f5dd920715b52bc58f0eb58ce7dc48ba0d
Mark14444 <mih.mark2015@gmail.com>
2022-08-22 21:53:20 +0300

    Update App.js

--- Fragments/src/App.js
+++ Fragments/src/App.js
@@ -5,24 +5,23 @@ import RegistrationForm from "./components/registration/RegistrationForm";
 import SignUpForm from "./components/SignUp/SignUpForm";
 import AdminPage from "./components/admin/AdminPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NotificationPage from './components/notification/NotificationPage';
-import {getUser} from './services/userService';
+import NotificationPage from "./components/notification/NotificationPage";
+import { getUser } from "./services/userService";
 function App() {
   const [user, setUser] = useState();
 
-  useEffect(()=>{
-    const token = window.localStorage.getItem('token');
-    if(!user)
-      getUser(token, setUser)
-  },[user]);
-  
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    if (!user) getUser(token, setUser);
+  }, [user]);
+
   return (
     <Router>
-      <Header user = {user}/>
+      <Header user={user} />
       <Routes>
-        <Route path="/Sign-up" element = {<SignUpForm/>}/>
+        <Route path="/Sign-up" element={<SignUpForm />} />
         <Route path="/Registration" element={<RegistrationForm />} />
-        <Route path="/Notifications" element = {<NotificationPage/>}/>
+        <Route path="/Notifications" element={<NotificationPage />} />
         <Route path="/api/Users" element={<Profile />} />
         <Route path="/Admin" element={<AdminPage />} />
       </Routes>
